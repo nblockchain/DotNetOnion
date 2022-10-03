b3e4f572b3369cde79fd2f3b11c7bb0567f4e76b
Andres G. Aragoneses <knocte@gmail.com>
2022-10-03 15:07:36 +0800

    fixup WIP6

--- NOnion/Cells/Relay/RelayEstablishIntro.fs
+++ NOnion/Cells/Relay/RelayEstablishIntro.fs
@@ -136,7 +136,7 @@ type RelayEstablishIntro =
                         ((RelayIntroExtension.FromBytes reader) :: state)
                         (remainingCount - 1uy)
 
-            readExtensionsList List.empty extensionCount
+            readExtensionsList List.empty extensionCount |> Seq.rev
 
         let handshakeAuth = reader.ReadBytes authKey.MacLength
 
